efb692ecc1fbaa3282e229d410bef03506288ac5
rouz majlessi <rmajlessi@gmail.com>
2017-06-17 16:51:30 -0700

    add new Model class for Genre search, has there 3 properties  a list, a selected list and a genre for selected list

---
+++ MvcMovies/Models/MovieGenreViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovies.Models
+{
+    public class MovieGenreViewModel
+    {
+        // contains a list of movies
+        public List<Movie> movies;
+
+        // containg a list of genres
+        public SelectList genres;
+
+        //contains selected genre
+        public string movieGenre { get; set; }
+    }
+}
