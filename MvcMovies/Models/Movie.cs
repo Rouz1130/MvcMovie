9b04bab77e630ee0474f002c621bcee6ecc42c87
rouz majlessi <rmajlessi@gmail.com>
2017-06-17 18:31:47 -0700

    add valadation to movie class model required, rexpression, minlenght

--- MvcMovies/Models/Movie.cs
+++ MvcMovies/Models/Movie.cs
@@ -7,13 +7,29 @@ namespace MvcMovies.Models
     {
         // type 'prop' hit tab twice shortcut for writing properties.
         public int ID { get; set; } //ID is primary key
+
+        [StringLength(60, MinimumLength =3)]
+        [Required]
         public string Title { get; set; }
 
         [Display(Name = "Release Date")] //Display attribute specifies what to display for the name of a field(in our case 'Release Date') instead of ReleaseDate.
         [DataType(DataType.Date)]       // DataType attribute dpecifies the type of the Date , therefore the time information is not displayed.
         public DateTime ReleaseDate { get; set; }
-        public string Genre { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; } //Not an int because using more that just Whole Numbers.
+
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; }
+
+
+        [RegularExpression(@"[A-Z]+[a-zA-Z''-'\s]*$")]
+        [StringLength(5)]
+        [Required]
         public string Rating { get; set; }
     }
 }
